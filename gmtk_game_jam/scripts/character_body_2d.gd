c187d4469c9eee35eedad1aaeba99ae1afeb2975
signalfern <signalfern.dev@gmail.com>
2025-08-01 08:31:24 +0530

    added MC movement
    made the character move in all directions.

---
+++ gmtk_game_jam/scripts/character_body_2d.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+@export var Speed = 300.0
+
+func get_input():
+	var input_direction = Input.get_vector("left","right","up","down")
+	velocity = input_direction * Speed
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
